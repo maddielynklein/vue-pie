3a31e59037d924b3dfec1d7a242380617abf3697
Maddie <maddielynklein@gmail.com>
2019-11-03 19:11:38 -0700

    Adding demos and start of readme

---
+++ README.md
@@ -0,0 +1,30 @@
+# vue-pie
+
+## Add to project
+```
+npm install vue-pie
+```
+
+## Description
+Vue pie chart component built with d3
+
+## Project Dependencies
+d4
+
+## Props
+| Prop | Required | Description | Default |
+| --- | --- | ----------- | ----------- |
+
+
+## Examples
+![Pie](demo/pie.png)
+
+![Basic](demo/basic.gif)
+
+![Hover](demo/hover.gif)
+
+![Click](demo/click.gif)
+
+![Timer](demo/timer.gif)
+
+![Spinner](demo/spinner.gif)
--- src/TestApp.vue
+++ src/TestApp.vue
@@ -2,10 +2,11 @@
   <div style="width: 75%;margin: 4em">
      <!-- <pie-chart legendLocation="left" :chartData="pie" :innerRadiusPercent="0.7" :endAngle="1.5 * Math.PI"
     :startAngle="-.5 * Math.PI" :hoverAnimation="true" :maxSelectedSections="2" :selectIdentifier="(d,i) => ['First','Second','Third','Fourth'][i]"/> -->
-    <!-- <pie-chart :chartData="pie" /> -->
+    <pie-chart :chartData="pie" />
     <!-- <pie-chart :chartData="{a:3,b:9,c:2,d:6,e:4}" :sectionKeys="['a','b','c','d','e']" :innerRadiusPercent="0.6"
     legendLocation="right" :formatLegend="(_,id) => 'Section ' + id.toUpperCase()" :hoverAnimation="true"
     :selectColor="scheme3"/> -->
+    <!-- <pie-chart :chartData="[1,1,1]" :padAngle="0.05 * Math.PI" :innerRadiusPercent="0.7" :endAngle="endAngle" :startAngle="startAngle" :transitionDuration="1000"/> -->
   </div>
 </template>
 
@@ -20,14 +21,20 @@
     },
     data() {
       return {
-        pie: [1,2,3,4]
+        pie: [1,2,3,4],
+        endAngle: 0,
+        startAngle: -.65 * Math.PI
       }
     },
     mounted() {
-      setTimeout((() => {
-        this.pie = [4,1,7]
-        // this.pie.push(5)
-      }).bind(this),2000)
+      // setTimeout((() => {
+      //   this.pie = [4,1,7]
+      //   // this.pie.push(5)
+      // }).bind(this),2000)
+      // d3.interval((function(){
+      //   this.endAngle += 1 * Math.PI
+      //   this.startAngle += 1 * Math.PI
+      // }).bind(this), 500)
     },
     methods: {
       scheme3(d) {
