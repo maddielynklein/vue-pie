ec04ba5326340982400e9bbd09c2ad9605eecd1d
Maddie <maddielynklein@gmail.com>
2019-11-03 20:41:48 -0700

    Adding html demo

--- src/TestApp.vue
+++ src/TestApp.vue
@@ -1,7 +1,43 @@
 <template>
   <div style="width: 75%;margin: 4em">
-     <pie-chart legendLocation="top" :chartData="pie" :innerRadiusPercent="0.7" :endAngle="1.5 * Math.PI"
-    :startAngle="-.5 * Math.PI" :hoverAnimation="true" :maxSelectedSections="2" :selectIdentifier="(d,i) => ['First','Second','Third','Fourth'][i]"/>
+     <!-- <pie-chart legendLocation="top" :chartData="pie" :innerRadiusPercent="0.7" :endAngle="1.5 * Math.PI"
+    :startAngle="-.5 * Math.PI" :hoverAnimation="true" :maxSelectedSections="2" :selectIdentifier="(d,i) => ['First','Second','Third','Fourth'][i]"/> -->
+    <pie-chart
+      :chartData="{
+        a: {
+          val: 300,
+          col: '#62d2e3',
+          label: 'React'
+        },
+        b: {
+          val: 700,
+          col: '#d5f28d',
+          label: 'Vue'
+        },
+        c: {
+          val: 400,
+          col: '#f2928d',
+          label: 'Angular'
+        },
+        d: {
+          val: 150,
+          col: '#9c8df2',
+          label: 'ExtJs'
+        },
+        e: {
+          val: 475,
+          col: '#f2e88d',
+          label: 'jQuery'
+        }
+      }"
+      :sectionKeys="['a','b','c','d','e']"
+      :selectValue="(d) => d.val"
+      :selectColor="(d) => d.col"
+      :innerRadiusPercent="0.3"
+      legendLocation="left"
+      :formatLegend="(d) => d.label"
+      :maxSelectedSections="1"
+    />
     <!-- <pie-chart :chartData="pie" /> -->
     <!-- <pie-chart :chartData="{a:3,b:9,c:2,d:6,e:4}" :sectionKeys="['a','b','c','d','e']" :innerRadiusPercent="0.6"
     legendLocation="right" :formatLegend="(_,id) => 'Section ' + id.toUpperCase()" :hoverAnimation="true"
